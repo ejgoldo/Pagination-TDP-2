3c76a76a7f031a4b6601004913798768b42047d5
EJ Serafin <edgardo@chainblx.com>
2018-10-18 12:49:58 -0700

    added the search function ex
    shooting for the exceeds grade so in turn added the ex function to base off of later

--- js/script.js
+++ js/script.js
@@ -49,3 +49,22 @@ function appendPageLinks(/* take a student list as an argument */) {
 
 
 
+//exceeds
+//search functionality
+function searchList() {
+    // Obtain the value of the search input
+    // remove the previous page link section    
+    // Loop over the student list, and for each student…
+// ...obtain the student’s name…
+// ...and the student’s email…
+// ...if the search value is found inside either email or name…
+    		// ...add this student to list of “matched” student
+    // If there’s no “matched” students…
+           // ...display a “no student’s found” message
+    // If over ten students were found…
+           // ...call appendPageLinks with the matched students
+   // Call showPage to show first ten students of matched list
+}
+
+
+
