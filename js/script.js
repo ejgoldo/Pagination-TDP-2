9a0f8e6e59421beac6daef959ccb0ac708042d7d
EJ Serafin <edgardo@chainblx.com>
2018-10-18 12:38:23 -0700

    added ex functions
    added the function examples needed for showing each page and adding the page links.
    will use it as a reference to build each function

--- js/script.js
+++ js/script.js
@@ -12,11 +12,31 @@ let pageLinks = 0;
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+function showPage(page, $studentList) {
+    // first hide all students on the page
+    $studentList.hide();
+    // Then loop through all students in our student list argument
+    for (let i=0; i <= $studentList.length; i++) {
+       // if student should be on this page number
 
+           // show the student
+    }
+ }
 
 
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
+function appendPageLinks(/* take a student list as an argument */) {
+    // determine how many pages for this student list 
+    // create a page link section
+    // “for” every page
+        // add a page link to the page link section
+    // remove the old page link section from the site
+    // append our new page link section to the site
+    // define what happens when you click a link
+        // Use the showPage function to display the page for the link clicked
+        // mark that link as “active”
+}
 
 
 
