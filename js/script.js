e7c44f85ca78eae49f60f0af99d3c9019223338d
EJ Serafin <edgardo@chainblx.com>
2018-10-25 23:42:33 -0700

    cleaned comments, fixed functions and event listeners

--- js/script.js
+++ js/script.js
@@ -3,53 +3,55 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// student information put into variables, one to contain them and the other to put them into pages and/or a list. also, put the student-search div and pagination div into variables
+// student information put into variables, one to contain them and the other to put them into pages and/or a list. also, put the student-search div and pagination div into variables while appending the searchDiv into its place in the HTML
 let studentInfo = $('.student-item');
 let searchDiv ='<div class="student-search"><input id="search" placeholder="Search for students..."><button>Search</button></div>';
 var pagDiv ='<div class="pagination"><ul></ul></div>';
 var studentList = pages(studentInfo);
 
+$('.page-header.cf').append(searchDiv);
+
 
 //grabs the list of students and puts it into an array. making it easier to select elements from the list in future functions
 function pages(list) {
-    let originalList = list.slice();
-    let arrayPages = [];
-    while (originalList.length) {
-        arrayPages.push(originalList.splice(0, 10));
-    }
-    return arrayPages;
+	let originalList = list.slice();
+	let pagesArr = [];
+	while (originalList.length) {
+		pagesArr.push(originalList.splice(0,10));
+	}
+	return pagesArr;
 }
 
 // only displays the first page of the array by using the previous pages(list) function
-function showPage(pageNum, pagesOfList) {
+function showPage(pageNumber, pageList) {
     $('.student-list li').hide();
-    $.each(pagesOfList, function(index, page){
-        if (pageNum === index) {
+    $.each(pageList, function(index, page){
+        if (pageNumber === index) {
             $.each(page, function(i, listItem){
-                $(listItem).show();
+                $(listItem).fadeIn('fast');
             });
         }
     });
  }
 
- function appendPageLinks(pagesOfList) {
+ function appendPageLinks(pageList) {
     //  dynamically create the pagination div and ul
-     $('.page').append('<div class="pagination"></div>');
-     $('.pagination').append('<ul>');
+     $('.page').append(pagDiv);
     // number of pages is determined by the length of pagesOfList
-    let numberOfPages = pagesOfList.length;
+    var numberOfPages = pageList.length;
     // in this for loop a page or li is added based on the numberOfPages variable
-    for (let i = 0; i <= numberOfPages; i++) {
-        $('.pagination ul').append('<li><a href="#">'+ i +'</a></li>')
+    for (var i = 0; i <= numberOfPages; i++) {
+        var buttons = '<li><a href="#">'+ i +'</a></li>';
+        $('.pagination ul').append(buttons);
     }
     // add the active class to the first a tag of the list
     $('.pagination ul li a').first().addClass('active');
 
     // click declares which page becomes active by receiving the active class
     $('.pagination ul li a').on('click', function(e) {
-        let pageSelected = parseInt($(this)[0].text) - 1;
+        var pageSelected = parseInt($(this)[0].text) - 1;
         // showPage displays the page that is clicked and adds the active class
-        showPage(pageSelected, pagesOfList);
+        showPage(pageSelected, pageList);
         $('.pagination ul li a').removeClass();
         // after class is removed from the starting page through a click, the clicked page becomes active
         $(this).addClass('active');
@@ -87,5 +89,5 @@ appendPageLinks(studentList);
 showPage(0, studentList);
 
 // event listeners for search functionality
-$('.student-search').find('button').on('click', studentList);
-$('.student-search').find('input').keyup(studentList);
+$('.student-search').find('button').on('click', searchList);
+$('.student-search').find('input').keyup(searchList);
